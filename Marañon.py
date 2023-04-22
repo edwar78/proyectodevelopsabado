9796feb1e47de9a3162f02c75f3bc9a5a1fa1480
401 <gialarcont@gmail.com>
2023-04-22 13:08:23 -0500

    djghjkshdjk

--- Marañon.py
+++ Marañon.py
@@ -1,10 +1,48 @@
-class Marañom:
+class Marañon:
     
     def __init__(self):
-        self.nombre=None
-        self.catidad=None
-        self.precio=None
-        self.vitaminaAportada=None
+        self.__nombre=None
+        self.__catidad=None
+        self.__precio=None
+        self.__vitaminaAportada=None
     
+    
+    
+    @property    
+    def vitaminaAportada(self):
+        return self.__vitaminaAportada
+    #declrando el metodo ser(confihguro/llevo un valor a un atrubuto)
+    @vitaminaAportada.setter
+    def vitaminaAportada(self,dato):
+        self.__vitaminaAportada=dato
+    
+     
+    @property    
+    def precio(self):
+        return self.__precio
+    #declrando el metodo ser(confihguro/llevo un valor a un atrubuto)
+    @precio.setter
+    def precio(self,dato):
+        self.__precio=dato   
+    
+    @property    
+    def catidad(self):
+        return self.__catidad
+    #declrando el metodo ser(confihguro/llevo un valor a un atrubuto)
+    @catidad.setter
+    def catidad(self,dato):
+        self.__catidad=dato
+        
+    
+
+        #DECLARAN EL METODO GET (OBTEGO/MUESTRO UN ATRUBURO)
+    @property    
+    def nombre(self):
+        return self.__nombre
+    #declrando el metodo ser(confihguro/llevo un valor a un atrubuto)
+    @nombre.setter
+    def nombre(self,dato):
+        self.__nombre=dato
+        
     def agregarFruta(self):
         print("Se pica y se macera con azucar morena...")
---
+++ main.py
@@ -0,0 +1,18 @@
+from Marañon import Marañon
+
+objeto=Marañon()
+objeto.nombre="Marañon CH"
+print(objeto.nombre)
+
+
+objeto=Marañon()
+objeto.catidad="100kg de Marañon CH"
+print(objeto.catidad)
+
+objeto=Marañon()
+objeto.precio="1000$ la onsa de Marañon CH"
+print(objeto.precio)
+
+objeto=Marañon()
+objeto.vitaminaAportada="fuerza de hulk sin ponerse verde, Marañon CH"
+print(objeto.vitaminaAportada)
